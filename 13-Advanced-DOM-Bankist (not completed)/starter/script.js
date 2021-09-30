556073bb4f4afc7f25aabd2b07d9b05af265918c
Jonathan Koziel <tweggy4@gmail.com>
2021-09-30 21:12:18 +0100

    Selecting, Creating and Deleting elements

--- 13-Advanced-DOM-Bankist (not completed)/starter/script.js
+++ 13-Advanced-DOM-Bankist (not completed)/starter/script.js
@@ -29,3 +29,50 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/** Lectures **/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Selecting, Creating and Deleting elements
+ */
+
+// Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and Inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies for improved functionality and anaytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and anaytics. <button class="btn btn--close-cookie">Got it!</button';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elments
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message);
+  });
