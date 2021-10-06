dc996253e29ff76019e255d7d87d8e209bf99444
Jonathan Koziel <tweggy4@gmail.com>
2021-10-06 21:47:41 +0100

    Event Propagation in Practice #1

--- 13-Advanced-DOM-Bankist (not completed)/starter/script.js
+++ 13-Advanced-DOM-Bankist (not completed)/starter/script.js
@@ -169,3 +169,22 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 // h1.onmouseenter = function (e) {
 //   alert('onmouseenter: Great! You are reading the heading :D');
 // };
+
+/**
+ * Event Propagation in Practice
+ */
+
+// rgb(255, 255, 255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector('.nav').addEventListener('click', function (e) {});
