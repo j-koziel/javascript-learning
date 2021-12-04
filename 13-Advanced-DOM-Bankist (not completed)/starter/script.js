6c9d084336e49c55779d0e95ffb5d5816ad24100
Jonathan Koziel <tweggy4@gmail.com>
2021-12-04 20:44:11 +0000

    Revealing elements on scroll

--- 13-Advanced-DOM-Bankist (not completed)/starter/script.js
+++ 13-Advanced-DOM-Bankist (not completed)/starter/script.js
@@ -160,6 +160,28 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /** Lectures **/
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -174,7 +196,7 @@ headerObserver.observe(header);
 // console.log(document.body);
 
 // const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
+// const allSections = document.querySelectorAll('.section');
 
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
