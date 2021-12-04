a3a6dd1afe459bf6da9284f482f9dead384520ae
Jonathan Koziel <tweggy4@gmail.com>
2021-12-04 19:57:23 +0000

    Update script.js

--- 13-Advanced-DOM-Bankist (not completed)/starter/script.js
+++ 13-Advanced-DOM-Bankist (not completed)/starter/script.js
@@ -119,6 +119,19 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// Sticky navigation
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+
+// window.addEventListener('scroll', () => {
+//   console.log(window.scrollY);
+
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+
+// Sticky navigation: Intersection Observer API
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /** Lectures **/
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
