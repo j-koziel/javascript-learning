bd91ea2ad63a078c96bf34e3ce5d00f6b93d6232
Jonathan Koziel <tweggy4@gmail.com>
2021-12-07 18:46:14 +0000

    Prototypes

--- 14-OOP (not completed)/starter/script.js
+++ 14-OOP (not completed)/starter/script.js
@@ -25,3 +25,27 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.spieces = 'Homo Sapiens';
+console.log(jonas.spieces, matilda.spieces);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
