70bd638393ae7e30bf6260e3d974cd4a35b5aa0d
Jonathan Koziel <tweggy4@gmail.com>
2021-12-07 18:26:52 +0000

    constructor functions and new operator

--- 14-OOP (not completed)/starter/script.js
+++ 14-OOP (not completed)/starter/script.js
@@ -1,5 +1,27 @@
 'use strict';
 
 // Constructor Functions and the new Operator
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
 
-const Person = function (firstName, birthYear) {};
+  // Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. Function is called, this = {}
+// 3. {} is linked to prototype
+// 4. function automatically returns {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
