1700f24d664ffc627a53ad81662dc3e69a35661c
Jonathan Koziel <tweggy4@gmail.com>
2021-12-11 14:42:59 +0000

    Setters and Getters

--- 14-OOP (not completed)/starter/script.js
+++ 14-OOP (not completed)/starter/script.js
@@ -131,8 +131,8 @@ mercedes.brake();
 
 // class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -142,13 +142,31 @@ class PersonCl {
   } // No commas in between methods
 
   greet() {
-    console.log(`Hey ${this.firstName}`);
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  // Setters and Getters
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
   }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -162,6 +180,26 @@ jessica.greet();
 // 2. Class are first-class citizens
 // 3. Classes are executed in strict mode
 
+const walter = new PersonCl('Walter White', 1965);
+
 /**
  * Setters and Getters
  */
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
