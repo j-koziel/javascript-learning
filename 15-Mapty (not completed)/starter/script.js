b712e83227a86fa8e2f75cbf4f5e606b015efba0
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2021-12-20 19:28:34 +0000

    leaflet library pt.2

--- 15-Mapty (not completed)/starter/script.js
+++ 15-Mapty (not completed)/starter/script.js
@@ -25,7 +25,7 @@ if (navigator.geolocation)
 
       const map = L.map('map').setView(coords, 13);
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
@@ -39,5 +39,3 @@ if (navigator.geolocation)
       alert('Could not get your position');
     }
   );
-
-console.log(firstName);
