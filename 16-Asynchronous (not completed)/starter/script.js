4c7a4e6e349d62293030ab04c32505e4aeaa8695
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2021-12-26 10:14:23 +0000

    Chaining Promises

--- 16-Asynchronous (not completed)/starter/script.js
+++ 16-Asynchronous (not completed)/starter/script.js
@@ -141,7 +141,17 @@ setTimeout(() => {
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('poland');
