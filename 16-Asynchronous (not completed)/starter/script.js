c7b4dda3db954dbf11b77f791df7a40633f7dc23
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-08 11:26:26 +0000

    Running Promises in Parallel

--- 16-Asynchronous (not completed)/starter/script.js
+++ 16-Asynchronous (not completed)/starter/script.js
@@ -451,6 +451,7 @@ createImage('./img/img-1.jpg')
 /**
  * Consuming Promises with Async/Await
  */
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -502,3 +503,24 @@ console.log('1: WILL get location');
   }
   console.log('3: Finished getting location');
 })();
+*/
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('poland', 'canada', 'tanzania');
