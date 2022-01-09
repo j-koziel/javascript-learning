efeedb1349c61bbcfb2c4f815df6cfccb900388a
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-09 14:57:38 +0000

    The Module Pattern

--- 17-Modern-JS-Modules-Tooling (not completed)/starter/script.js
+++ 17-Modern-JS-Modules-Tooling (not completed)/starter/script.js
@@ -25,7 +25,7 @@ console.log(cart);
 // const data = await res.json();
 // console.log(data);
 // console.log('Finsihed fetching');
-
+/*
 const getLastPost = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
@@ -41,3 +41,33 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
