2945a2c73d2b68e8b1d420d4c9cfa221399e4f3f
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-10 10:49:44 +0000

    Introduction to NPM + Cleaning up folders

--- 17-Modern-JS-Modules-Tooling (not completed)/script.js
+++ 17-Modern-JS-Modules-Tooling (not completed)/script.js
@@ -3,7 +3,7 @@
 // addToCart('bread', 5);
 // console.log(price, tq);
 
-console.log('Importing module');
+// console.log('Importing module');
 // console.log(shippingCost);
 
 // import * as ShoppingCart from './shoppingCart.js';
@@ -84,3 +84,26 @@ console.log(ShoppingCart2.shippingCost);
 
 // Import
 // const { addTocart } = require('./shoppingCart.js');
+
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
