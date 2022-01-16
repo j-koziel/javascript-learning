82b6f0b91d3b8d1ab88a814d37f2ae378b75d865
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-16 18:18:35 +0000

    Publisher-Subscriber pattern

--- 18-forkify (not completed)/src/js/controller.js
+++ 18-forkify (not completed)/src/js/controller.js
@@ -6,14 +6,6 @@ import recipeView from './views/recipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -35,9 +27,7 @@ const controlRecipes = async function () {
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
-
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
