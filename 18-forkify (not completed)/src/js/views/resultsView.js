9c58324d3385a349f34193694808a52bed361635
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-23 15:36:09 +0000

    fixed the error

--- 18-forkify (not completed)/src/js/views/resultsView.js
+++ 18-forkify (not completed)/src/js/views/resultsView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
-  _parentElement = document.querySelector('.search-results');
+  _parentElement = document.querySelector('.results');
   _errorMessage = 'No recipes found for your query! Please try again ;)';
   _message = '';
 
