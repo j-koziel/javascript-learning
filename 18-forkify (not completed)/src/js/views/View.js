f7d7bd91c75bde6d671fd128e07c8de223aef670
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-25 19:36:47 +0000

    problem - servings still updating

--- 18-forkify (not completed)/src/js/views/View.js
+++ 18-forkify (not completed)/src/js/views/View.js
@@ -13,6 +13,15 @@ export default class View {
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
+  update(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+    console.log(newMarkup);
+  }
+
   _clear() {
     this._parentElement.innerHTML = '';
   }
