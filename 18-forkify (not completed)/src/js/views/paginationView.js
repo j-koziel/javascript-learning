16f99e85054401674ed97e4b24167e4920a0f8c5
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-29 19:52:51 +0000

    Storing boookmarks with localStorage

--- 18-forkify (not completed)/src/js/views/paginationView.js
+++ 18-forkify (not completed)/src/js/views/paginationView.js
@@ -7,7 +7,6 @@ class PaginationView extends View {
   addHandlerClick(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--inline');
-      console.log(btn);
 
       if (!btn) return;
 
@@ -22,7 +21,6 @@ class PaginationView extends View {
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
-    console.log(numPages);
 
     // Page 1, and there are other pages
     if (curPage === 1 && numPages > 1) {
