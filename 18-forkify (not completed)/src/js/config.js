2bdc802c92fa9f33f8f6c12ca850d575fc62190c
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-30 16:07:09 +0000

    Uploading a New Recipe - Pt. 2

--- 18-forkify (not completed)/src/js/config.js
+++ 18-forkify (not completed)/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'a02c6cd4-8dbd-4e9d-af48-2ec51cd1d408';
+const MODAL_CLOSE_SEC = 5;
