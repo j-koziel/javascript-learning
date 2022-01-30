d366082f58b6196b3df7731cda37760a445cc28e
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-30 17:32:49 +0000

    Simple Deployment With Netlify

--- 18-forkify (not completed)/src/js/config.js
+++ 18-forkify (not completed)/src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'a02c6cd4-8dbd-4e9d-af48-2ec51cd1d408';
-const MODAL_CLOSE_SEC = 5;
+export const MODAL_CLOSE_SEC = 5;
