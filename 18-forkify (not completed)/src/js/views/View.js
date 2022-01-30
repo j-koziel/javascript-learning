88d48cb79ac7afd789b749c7b48c8e5865a6587c
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-01-30 17:07:03 +0000

    Wrapping up

--- 18-forkify (not completed)/src/js/views/View.js
+++ 18-forkify (not completed)/src/js/views/View.js
@@ -3,6 +3,15 @@ import icons from 'url:../../img/icons.svg'; // Parcel 2
 export default class View {
   _data;
 
+  /**
+   * Render the received objecy to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true]  If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render is false
+   * @this {Object} View instance
+   * @author Jonathan Koziel
+   * @todo finish implementation
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
